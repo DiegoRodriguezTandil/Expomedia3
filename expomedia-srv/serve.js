6db7429f6528b94a69268bbd5d414aef6013c7d4
Diego Rodriguez <diegorodriguez@MacBook-Pro-de-Diego.local>
2016-11-28 21:32:53 -0300

    Making changes in te server to accept origin *

--- expomedia-srv/serve.js
+++ expomedia-srv/serve.js
@@ -10,40 +10,24 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname+'expomedia-cli/dist'));
 
-console.log(__dirname+'/expomedia-cli/dist/');
+// app.use(express.static(__dirname+'expomedia-cli/dist'));
+// console.log(__dirname);
 
-
-/* ARCHIVOS */
-app.get('/inline.bundle.js', function (req, res) {
-    res.sendFile(path.join(__dirname,'/expomedia-cli/dist/inline.bundle.js'));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
 });
 
-app.get('/styles.bundle.js', function (req, res) {
-    res.sendFile(path.join(__dirname,'/expomedia-cli/dist/styles.bundle.js'));
-});
-
-app.get('/main.bundle.js', function (req, res) {
-    res.sendFile(path.join(__dirname,'/expomedia-cli/dist/main.bundle.js'));
-});
-
-app.get('/*', function (req, res) {
-/*
-    // if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if(accept !== 'html'){
-        next();
-    }
-
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== ''){
-        next();
-    }
-*/
-    console.log(req.accepts('html', 'json', 'xml'));
-    res.sendFile(path.join(__dirname,'/expomedia-cli/dist/index.html'));
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 
+        a: 1,
+        b: 3,
+        c: "sdadasdasd" 
+    }));
 });
 
 // catch 404 and forward to error handler
@@ -58,3 +42,56 @@ app.listen(process.env.PORT, function () {
 });
 
 module.exports = app;
+
+
+/* 
+
+
+
+
+
+
+
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var appRoutes = require('./routes/app');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+
+
+module.exports = app;
+
+
+ */
