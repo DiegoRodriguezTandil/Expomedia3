bd5fb0ef8dc6cda468fd7ef28c105ba715799711
Diego Rodriguez <diego@qwavee.com>
2017-01-07 16:02:25 -0300

    Adding libs

---
+++ app/alas.map.js
@@ -0,0 +1,6 @@
+(function($) {
+
+  //start a timer
+  $("#div-id").timer();
+
+}());
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <script type="text/javascript" src="client/dev/bower_components/jquery/dist/jquery.min.js"></script>
+        <script type="text/javascript" scr="client/dev/bower_components/timer.jquery/dist/timer.jquery.js"></script>
+        <script type="text/javascript" scr="app/alas.maps.js"></script>
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+        <div id="div-id"></div>
+    </body>
+</html>
