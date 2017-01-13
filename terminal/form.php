63395ff48df22c1db6cb3183bd2fb1b382ed1c18
koqui <koquipizzi@gmail.com>
2017-01-13 15:14:38 -0300

    agreado de validadiones

--- terminal/form.php
+++ terminal/form.php
@@ -10,39 +10,39 @@
             
             <script src="app/js/mainComp.js"></script>
     </head>
-  <body>
+    <body>
     
     <div class="container">
-    <h2>Crear / Actualizar movimiento </h2>
-      <div class="row">
-        <div class="col-lg-12"></div>
-      </div>
-    </div>
-    <div class="container" >
-      <div class"row">
-   <?php
+        <h2>Crear / Actualizar movimiento </h2>
+        <div class="row">
+            <?php
+            if (!isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $action = "index.php?r=crear";
+            } else {
+                $action = 'index.php?r=actualizar&id=' . $id . "'";
+            }
+            ?>
+            <form id="meetingForm" class="form-horizontal"
+                  data-fv-framework="bootstrap"
+                  data-fv-icon-valid="glyphicon glyphicon-ok"
+                  data-fv-icon-invalid="glyphicon glyphicon-remove"
+                  data-fv-icon-validating="glyphicon glyphicon-refresh">
+                <div clas="col-md-6 col-md-offset-6" style="margin-top:5%; margin-left:15%;">
 
-     if(!isset($_GET['id'])){
-        $id=$_GET['id'];
-            $action= "index.php?r=crear";
-    }else{
-          $action= 'index.php?r=actualizar&id='.$id."'";
-        }        
-
-    ?>
-
-          <div clas="col-md-6 col-md-offset-6" style="margin-top:5%; margin-left:15%;">
-                <form class="col-lg-10" action=<?=$action?> >
-                 <div class="row">
+                    <div class="row">
 
                         <div class="col-lg-2">
                             <label for="Hora">Hora</label>
-                            <input type="text" class="form-control" id="Hora" placeholder="Ingrese un Horario">
-                         </div>
+                            <!--<input type="text" class="form-control" id="Hora" placeholder="Ingrese un Horario">-->
+                            <input type="text" class="form-control" name="time" placeholder="HH:mm"
+                                   pattern="^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$" id="Hora"
+                                   data-fv-regexp-message="La hora debe ser de la forma 19:34" />
+                        </div>
                         <div class="col-lg-3">
                             <label for="empresa">Empresa</label>
                             <input type="text" class="form-control" id="Empresa" placeholder="Ingrese Nombre de la Empresa">
-                         </div>
+                        </div>
                         <div class="col-lg-3">
                             <label for="empresa">Procedencia</label>
                             <input type="text" class="form-control" id="Procedencia" placeholder="Ingrese Procedencia">
@@ -53,56 +53,59 @@
                         </div>
                     </div>  
                     <div class="row">                            
-                            <h3 style="margin-top:5%; margin-button:5%; ">Selecione los días </h3>
-                            <style type="text/css">
-                                .inputDias{
-                                    margin-left:10px;
-                                }
-                            </style>
-                            <div class="col-lg-1">
-                                <label for="dia-lunes">Lunes</label>
-                                <input type="checkbox" class="form-control" id="Dia-lunes">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Martes</label>
-                                <input type="checkbox" class="form-control" id="dia-martes">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Miércoles</label>
-                                <input type="checkbox" class="form-control" id="dia-miercoles">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Jueves</label>
-                                <input type="checkbox" class="form-control" id="dia-jueves">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Viernes</label>
-                                <input type="checkbox" class="form-control" id="dia-viernes">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Sabado</label>
-                                <input type="checkbox" class="form-control" id="dia-sabado">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Domingo</label>
-                                <input type="checkbox" class="form-control" id="dia-domingo">
-                            </div> 
-                            <div class="col-lg-1 inputDias">
-                                <label for="dia-">Feriado</label>
-                                <input type="checkbox" class="form-control" id="dia-feriado">
-                            </div>                                                                                                                                             
+                        <h3 style="margin-top:5%; margin-button:5%; ">Selecione los días </h3>
+                        <style type="text/css">
+                            .inputDias{
+                                margin-left:10px;
+                            }
+                        </style>
+                        <div class="col-lg-1">
+                            <label for="dia-lunes">Lunes</label>
+                            <input type="checkbox" class="form-control" id="Dia-lunes">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Martes</label>
+                            <input type="checkbox" class="form-control" id="dia-martes">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Miércoles</label>
+                            <input type="checkbox" class="form-control" id="dia-miercoles">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Jueves</label>
+                            <input type="checkbox" class="form-control" id="dia-jueves">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Viernes</label>
+                            <input type="checkbox" class="form-control" id="dia-viernes">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Sabado</label>
+                            <input type="checkbox" class="form-control" id="dia-sabado">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Domingo</label>
+                            <input type="checkbox" class="form-control" id="dia-domingo">
+                        </div> 
+                        <div class="col-lg-1 inputDias">
+                            <label for="dia-">Feriado</label>
+                            <input type="checkbox" class="form-control" id="dia-feriado">
+                        </div>                                                                                                                                             
 
-                      </div>  
+                    </div>  
                 </div>            
                 <div class="row">
-                     <div class="col-lg-12" style="margin-top:2%;  float: left;">
+                    <div class="col-lg-12" style="margin-top:2%;  float: left;">
                         <button type="submit" class="btn btn-default">Guardar</button>
                     </div>
-                 </div>
-              </form>
+                </div>
+            </form>
           </div>
-     </div>
-
-    </div><!-- /.container -->
+    </div>
   </body>
+  <script>
+$(document).ready(function() {
+    $('#meetingForm').formValidation();
+});
+</script>
 </html>
--- terminal/index.php
+++ terminal/index.php
@@ -1,23 +1,41 @@
 <html>
     <head> 
         <meta charset="utf-8">
+        <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700&subset=latin-ext' rel='stylesheet' type='text/css'>-->
+        <!--<link href="https://fonts.googleapis.com/css?family=Passion+One|Viga|Satisfy" rel="stylesheet" type='text/css'>-->
         <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-        <script type="text/javascript" src="app/js/viajes.js"></script>
+
+        <script type="text/javascript" src="bower_components/timer.jquery/dist/timer.jquery.js"></script>
+   
+        <link rel="stylesheet" href="app/css/material.indigo-pink.min.css">
+        <link rel="stylesheet" type="text/css" href="app/css/main_terminal.css"/>
         <script src="app/js/mainComp.js"></script>
+        <script type="text/javascript" src="app/js/viajes.js"></script>
     </head>
     <body>
         <div class="container">
-            <div id="top">                
-                <div id="textoTop" >
-                    <div class="text"> Terminal de Ómnibus de Tandil </div>
-                </div>
-            </div> 
-        </div>
-        <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
-        </div>
-        <script>
+            <top>
+                <img src="images/tandil_logo.png" style="margin-top: 0.3em;" />
+                <span>TERMINAL TANDIL - Movimiento diario
+                <div class="Timer"></div></span>
+            </top>
             
-           
-        </script>
+            <table class="table-striped table-mc-light-blue mdl-data-table mdl-js-data-table  mdl-shadow--2dp">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th class="mdl-data-table__cell--non-numeric">Hora</th>
+                        <th class="mdl-data-table__cell--non-numeric">Empresa</th>
+                        <th class="mdl-data-table__cell--non-numeric">Procedencia</th>
+                        <th class="mdl-data-table__cell--non-numeric">Destino</th>
+                    </tr>
+                </thead>
+                <tbody id="viajes">                    
+                    
+                </tbody>
+            </table>     
+            <footer>
+                <img src="images/logo-qwavee-blanco.png" width="200px" style="margin-top: 0.3em;"  />
+            </footer>
+        </div>   
     </body>
 </html>
