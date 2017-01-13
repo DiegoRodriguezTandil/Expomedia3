45ace2aaa0e475dbee299a261631d9221b0cf355
koqui <koquipizzi@gmail.com>
2017-01-13 12:32:33 -0300

    display

---
+++ terminal/app/js/mainComp.js
@@ -0,0 +1,91 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ function toggleFullScreen(elem) { 
+            if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+              if (elem.requestFullScreen) {
+                elem.requestFullScreen();
+              } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+              } else if (elem.webkitRequestFullScreen) {
+                elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+              } else if (elem.msRequestFullscreen) {
+                elem.msRequestFullscreen();
+              }
+            } else {
+              if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+              } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+              } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+              } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+              }
+            }
+          }
+
+$(document).ready(function(e) {
+            
+            
+        document.body.style.overflow = 'hidden';
+        
+        var start = new Date;
+
+        setInterval(function() { 
+            var minute = start.getMinutes();
+            var hour = start.getHours();
+            if (minute < 10)
+                minute = "0"+minute;
+            var hora = hour +":"+minute;
+            $('.Timer').text(hora);
+            start = new Date;
+        }, 1000);
+
+        $(".ideaboxNews").ideaboxNews({
+		/*	maxwidth		:'400px',
+			position		:'leftfixed',
+			openspeed		:'fast'*/
+		
+			maxwidth		:'100%',
+			newscount		:6,
+			position		:'leftfixed',
+			openspeed		:'fast',
+			headercolor		:'#FFFFFF',
+			headerbgcolor           :'rgba(74, 38, 106, 0.75)',
+		//	feedimage		:'img/in-rss-image.jpg',
+			feedimage		:'',
+			feedcount		:4,
+			feed			:'http://www.clarin.com/rss/lo-ultimo/,http://www.nacion.com/rss/latest/?contentType=NWS',
+			feedlabels		:'Noticias ALAS',
+			feedlink		:'self', //or target
+			feedupdatetimer	:60000  //60000 ms = 1 Sec
+		});
+		
+		$('#example5').ideaboxWeather({
+			/* lang		:"sp",
+			themecolor	:"#098da1",
+			location	:"Tandil",
+			daycount	:5,
+			metric		:'C',
+			days		:["Domingo", "Lunes", "Martes", "Mi�rcoles", "Jueves", "Viernes", "S�bado"],
+			dayssmall	:["Do", "Lo", "Ma", "Mi", "Ju", "Vi", "Sa"],
+			todaytext	:'Hoy', */
+			template	:"horizontal",
+			lang		:"sp",
+			themecolor	:"#098da1",
+			location	:"Tandil",
+			daycount	:6,
+			days		:["Domingo", "Lunes", "Martes", "Mi�rcoles", "Jueves", "Viernes", "S�bado"],
+			dayssmall	:["Dom", "Lu", "Mar", "Mi", "Ju", "Vi", "Sáb"],
+			metric		:'C',
+			imgpath		:"img/",
+			radius		:false
+		});
+                
+       
+    });
+    
---
+++ terminal/tele.php
@@ -0,0 +1,75 @@
+<html>
+    <head> 
+        <meta charset="utf-8">
+        <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700&subset=latin-ext' rel='stylesheet' type='text/css'>-->
+        <!--<link href="https://fonts.googleapis.com/css?family=Passion+One|Viga|Satisfy" rel="stylesheet" type='text/css'>-->
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+
+        <script type="text/javascript" src="bower_components/timer.jquery/dist/timer.jquery.js"></script>
+   
+        <link rel="stylesheet" href="app/css/material.indigo-pink.min.css">
+        <link rel="stylesheet" type="text/css" href="app/css/main_terminal.css"/>
+        <script src="app/js/mainComp.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <top>
+                <img src="images/tandil_logo.png" style="margin-top: 0.3em;" />
+                <span>TERMINAL TANDIL - Movimiento diario
+                <div class="Timer">TERMINAL TANDIL - Movimiento diario</div></span>
+            </top>
+            
+            <table class="table-striped table-mc-light-blue mdl-data-table mdl-js-data-table  mdl-shadow--2dp">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th class="mdl-data-table__cell--non-numeric">Hora</th>
+                        <th class="mdl-data-table__cell--non-numeric">Empresa</th>
+                        <th class="mdl-data-table__cell--non-numeric">Procedencia</th>
+                        <th class="mdl-data-table__cell--non-numeric">Destino</th>
+                    </tr>
+                </thead>
+                <tbody>                    
+                    <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>
+                                        <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>                    
+                    <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>                    
+                    <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>                   
+                    <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>
+                                        <tr>                        
+                        <td class="mdl-data-table__cell--non-numeric importante">12:25</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Empresa</td>
+                        <td class="mdl-data-table__cell--non-numeric">Procedendia1</td>
+                        <td class="mdl-data-table__cell--non-numeric importante">Destino</td>
+                    </tr>
+                </tbody>
+            </table>     
+            <footer>
+                <img src="images/logo-qwavee-blanco.png" width="200px" style="margin-top: 0.3em;"  />
+            </footer>
+        </div>   
+    </body>
+</html>
