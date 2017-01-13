2637a96fe183ff4ce43d1b85293762eec094def2
Diego Rodriguez <diego@qwavee.com>
2017-01-13 15:11:03 -0300

    Nuevas acciones de Guardar y Eliminar

--- terminal/form.php
+++ terminal/form.php
@@ -1,3 +1,46 @@
+<?php
+
+    $action = filter_input(INPUT_GET, "r", FILTER_SANITIZE_MAGIC_QUOTES, 
+            array("options" => array(
+                "default" => FALSE
+            ))
+    );
+    
+    $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_MAGIC_QUOTES, 
+            array("options" => array(
+                "default" => 'NULL'
+            ))
+    );
+    
+    
+    $row = [
+        'hora'=>'',
+        'empresa'=>'',
+        'procedencia'=>'',
+        'destino'=>'',
+        'dia'=> [
+                'lunes'=>'1',
+                'martes'=>'1',
+                'miercoles'=>'1',
+                'jueves'=>'1',
+                'viernes'=>'1',
+                'sabado'=>'1',
+                'domingo'=>'1',
+                'feriado'=>'1',
+        ]
+    ];
+    
+    if ($action && ($action = 'actualizar')) {
+        include_once 'funciones.php';
+        $arrayViajes = loadFromFile();
+        if(array_key_exists($id, $arrayViajes)){
+            $row = $arrayViajes[$id];
+        }
+        die('CLAVE NO EXISTE. NO ES POSIBLE MODIFICAR');
+    }
+
+
+?>    
 <html>    
     <head> 
         <meta charset="utf-8">
@@ -20,36 +63,24 @@
     </div>
     <div class="container" >
       <div class"row">
-   <?php
-
-     if(!isset($_GET['id'])){
-        $id=$_GET['id'];
-            $action= "index.php?r=crear";
-    }else{
-          $action= 'index.php?r=actualizar&id='.$id."'";
-        }        
-
-    ?>
-
           <div clas="col-md-6 col-md-offset-6" style="margin-top:5%; margin-left:15%;">
-                <form class="col-lg-10" action=<?=$action?> >
+              <form class="col-lg-10" method="GET" action=<?php echo "listar.php?r=guardar&id={$id}" ?> >
                  <div class="row">
-
                         <div class="col-lg-2">
                             <label for="Hora">Hora</label>
-                            <input type="text" class="form-control" id="Hora" placeholder="Ingrese un Horario">
+                            <input type="text" class="form-control" id="Hora" placeholder="Ingrese un Horario" value="<?php=$row['hora']?>" >
                          </div>
                         <div class="col-lg-3">
                             <label for="empresa">Empresa</label>
-                            <input type="text" class="form-control" id="Empresa" placeholder="Ingrese Nombre de la Empresa">
+                            <input type="text" class="form-control" id="Empresa" placeholder="Ingrese Nombre de la Empresa" value="<?php=$row['empresa']?>">
                          </div>
                         <div class="col-lg-3">
                             <label for="empresa">Procedencia</label>
-                            <input type="text" class="form-control" id="Procedencia" placeholder="Ingrese Procedencia">
+                            <input type="text" class="form-control" id="Procedencia" placeholder="Ingrese Procedencia" value="<?php=$row['procedencia']?>">
                         </div>
                         <div class="col-lg-3">
                             <label for="empresa">Destino</label>
-                            <input type="text" class="form-control" id="Destino" placeholder="Ingrese Destino">
+                            <input type="text" class="form-control" id="Destino" placeholder="Ingrese Destino" value="<?php=$row['destino']?>">
                         </div>
                     </div>  
                     <div class="row">                            
@@ -61,35 +92,35 @@
                             </style>
                             <div class="col-lg-1">
                                 <label for="dia-lunes">Lunes</label>
-                                <input type="checkbox" class="form-control" id="Dia-lunes">
+                                <input type="checkbox" class="form-control" id="Dia-lunes" <?php=$row['dia']['lunes']==1?'checked':''?> >
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Martes</label>
-                                <input type="checkbox" class="form-control" id="dia-martes">
+                                <input type="checkbox" class="form-control" id="dia-martes" <?php=$row['dia']['martes']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Miércoles</label>
-                                <input type="checkbox" class="form-control" id="dia-miercoles">
+                                <input type="checkbox" class="form-control" id="dia-miercoles" <?php=$row['dia']['miercoles']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Jueves</label>
-                                <input type="checkbox" class="form-control" id="dia-jueves">
+                                <input type="checkbox" class="form-control" id="dia-jueves" <?php=$row['dia']['jueves']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Viernes</label>
-                                <input type="checkbox" class="form-control" id="dia-viernes">
+                                <input type="checkbox" class="form-control" id="dia-viernes" <?php=$row['dia']['viernes']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Sabado</label>
-                                <input type="checkbox" class="form-control" id="dia-sabado">
+                                <input type="checkbox" class="form-control" id="dia-sabado" <?php=$row['dia']['sabado']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Domingo</label>
-                                <input type="checkbox" class="form-control" id="dia-domingo">
+                                <input type="checkbox" class="form-control" id="dia-domingo" <?php=$row['dia']['domingo']==1?'checked':''?>>
                             </div> 
                             <div class="col-lg-1 inputDias">
                                 <label for="dia-">Feriado</label>
-                                <input type="checkbox" class="form-control" id="dia-feriado">
+                                <input type="checkbox" class="form-control" id="dia-feriado" <?php=$row['dia']['feriado']==1?'checked':''?>>
                             </div>                                                                                                                                             
 
                       </div>  
---
+++ terminal/funciones.php
@@ -0,0 +1,87 @@
+<?php
+
+    function loadFromFile() {
+        $file = file_get_contents('horarios.json');
+        return json_decode($file,TRUE);
+    }    
+    
+    function saveToFile($horarios) {
+        file_put_contents('horarios.json', json_encode($horarios));
+    }    
+    
+    function ordenar($a, $b){
+        $ha = explode('##', $a);
+        $ha = $ha[0];
+        $hb = explode('##', $b);
+        $hb = $hb[0];
+
+        $dta = DateTime::createFromFormat("H:i", $ha);
+        $t = $dta->getTimestamp();
+        $dtb = DateTime::createFromFormat("H:i", $hb);
+        $t1 = $dtb->getTimestamp();
+        if($t < $t1) {
+            return -1;
+        } else if($t == $t1) {
+            return 0;
+        }
+        return 1;
+    }
+    
+    function loadToShow($horarios, $cant) {        
+        uksort($horarios, function ($a, $b){
+            $ha = explode('##', $a);
+            $ha = $ha[0];
+            $hb = explode('##', $b);
+            $hb = $hb[0];
+
+            $dta = DateTime::createFromFormat("H:i", $ha);
+            $t = $dta->getTimestamp();
+            $dtb = DateTime::createFromFormat("H:i", $hb);
+            $t1 = $dtb->getTimestamp();
+            if($t < $t1) {
+                return -1;
+            } else if($t == $t1) {
+                return 0;
+            }
+            return 1;
+        });  
+
+        date_default_timezone_set('America/Argentina/Buenos_Aires');        
+        $now = time();        
+        $r = array();
+        $manana = array();
+        
+        $i=0;
+        
+        while(($i<$cant)&&(count($horarios)>0))
+        {
+            $h = array_shift($horarios);
+            
+            $dta = DateTime::createFromFormat("H:i", $h['hora']);
+            $t = $dta->getTimestamp();
+            
+            if( $now <= $t )
+            {
+                array_push($r,$h);
+                $i++;
+            }
+            else
+            {
+                array_push($manana, $h);
+            }
+        }
+        
+        if($i<$cant)
+        {
+            while(($i<$cant)&&(count($manana)>0))
+            {
+                $h = array_shift($manana);
+                array_push($r,$h);
+                $i++;
+            }            
+        }
+        
+        return $r;
+        
+        
+    }
